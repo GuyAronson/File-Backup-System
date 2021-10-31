e6e7668239f0d0af3eb50e3bb693bedec726ab35
Guy Aronson <ahronson12@gmail.com>
2021-10-31 08:27:55 -0700

    31/10 17:30

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 import sys
 
+
 # def removing_breaklines(str):
 # 	lst = str.split('\\n')
 # 	string = ""
@@ -11,44 +12,80 @@ import sys
 
 
 def main():
-	#arguments from command line
-	ip = sys.argv[1]
-	port = sys.argv[2]
-	file_name = sys.argv[3]
+    # arguments from command line
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    file_name = sys.argv[3]
 
-	#defining a socket
-	s = socket(AF_INET, SOCK_DGRAM)
+    # defining a socket
+    s = socket(AF_INET, SOCK_DGRAM)
+    s.settimeout(10)
 
-	#Reading from a file
-	file = open(file_name, "r")
-	message = file.read()
-	chars_amount = len(message)
-	
-	i = 0
-	while i <= chars_amount:
-		#Taking the first 100 bytesq/chars from the file
-		small_message = message[i:i+100]
-		#print(small_message)
-		s.sendto(bytes(small_message, 'utf-8'), (ip, int(port)))
-		i += 100
 
-		data, addr = s.recvfrom(1024)
-		#new_data = str(data)
+    # Reading from a file as bytes
+    file = open(file_name, "rb")
+    message = file.read()
+    chars_amount = len(message)
 
-		#data = removing_breaklines(new_data[2:len(new_data)-1])
-		#print(data)
+    if chars_amount > 50000:
+        raise Exception("Too many bytes. More than 50k")
 
-		if data == bytes(small_message, 'utf-8'):
-			print("I got the same message i sent!")
-		else:
-			print("oh no, bad message")
-			break
-	
-	print("file has been trasnferred")
-	s.close()
+    drops = 0  # delete
+    #make sure the last package will be sent
+    if float(int(chars_amount / 96)) < chars_amount / 96:
+        packages_amount = int(chars_amount / 96) + 1
+    else:
+        packages_amount = int(chars_amount / 96)
+    package_number = 0
+    i = 0
+    is_first_message = True
+    single_message = ""
+    data = ""
 
-	return 0;
+    # the variable i runs on bytes in the text, increments by 100 each loop
+    while i < chars_amount:
+        if is_first_message:
+            # Sending the first message - contains the amount of packages will be sent.
+            single_message = int.to_bytes(packages_amount, 2, 'big')
+            s.sendto(single_message, (ip, int(port)))
+        else:
+            # Taking the first 100 bytes from the file
+            if package_number <= 9:
+                single_message = bytes(0) + bytes(0) + int.to_bytes(package_number, 2, 'big')
+            elif package_number <= 99:
+                single_message = bytes(0) + int.to_bytes(package_number, 2, 'big')
+            else:
+                single_message = int.to_bytes(package_number, 2, 'big')
+            single_message += message[i:i + 98]
+            i += 98
+            s.sendto(single_message, (ip, int(port)))
+
+        try:
+            data, addr = s.recvfrom(1024)
+        except OSError:
+            if not is_first_message:
+                i = i - 98
+
+        if data == single_message:
+            if is_first_message:
+                is_first_message = False
+                print("I got the same message i sent! (first message)")  # delete
+            else:
+                package_number += 1
+                print(f"I got the same message i sent! package: {package_number}")  # delete
+        else:  # delete
+            if not is_first_message:
+                i = i - 98
+            drops += 1  # delete
+            print(f'oh no, bad message, drop number {drops}')  # delete
+
+        print(str(i))
+
+    print("file has been transferred")
+    s.close()
+
+    return 0
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- server.py
+++ server.py
@@ -1,14 +1,49 @@
 import socket
 import sys
 
-def main():
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.bind(('', int(sys.argv[1])))
 
-	while True:
-		data, addr = s.recvfrom(1024)
-		print(str(data), addr)
-		s.sendto(data, addr)
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('', int(sys.argv[1])))
+
+    packages = []
+    packages_amount = 0
+
+    # Making sure that the first message with the amount of packages will arrive.
+    is_first_message = True
+    while is_first_message:
+        data, addr = s.recvfrom(1024)
+        packages_amount = int.from_bytes(data, "big")
+        is_first_message = False
+        s.sendto(data, addr)  # Sending an acknowledgment message.
+
+    print("packages_amount: " + str(packages_amount))
+    packages_received = 0
+
+    while packages_received < packages_amount:
+        data, addr = s.recvfrom(1024)
+        print("data: " + str(data))
+
+        package_num = int.from_bytes(data[0:2], 'big')  # Getting the package number - 3 first bytes.
+        message = data[2:]  # Saving the message without the package number
+        print("package num: " + str(package_num))
+        print("package rec: " + str(packages_received))
+        if package_num == packages_received:
+            packages.append(message)  # adding the message to the list
+            packages_received += 1  # increment the counter
+            s.sendto(data, addr)  # sending back the same message.
+        elif package_num == packages_received - 1:
+            # Which means we got the same message as before,
+            # we will just sent an acknowledgement message
+            s.sendto(data, addr)
+
+    # What if the server drops the last package - the client will not know that the server got it,
+    # The server finished his program, while client is still sending.
+
+    for st in packages:
+        print(str(st, 'utf-8'), end="")
+    print()
+
 
 if __name__ == "__main__":
-	main()
+    main()
